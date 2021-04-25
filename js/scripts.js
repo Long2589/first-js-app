b38fbc05a3bffbef2a4afcb43c48d8d4bf5d7deb
Long2589 <sarah.eliza.long@gmail.com>
2021-04-24 20:15:40 -0700

    Add Pokemon list information to scripts.js file

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,38 @@
-alert('Hello Wold');
-
-let favoriteFood = 'brussles sprouts';
-document.write(favoriteFood);
+let pokemonList = [
+	{
+		name: 'Charizard',
+		types: ['fire, flying'],
+		height: 1.7,
+		weight: 90.5
+	},
+	{
+		name: 'Seadra',
+	  types: ['water'],
+	  height: 1.2,
+	  weight: 25
+	},
+	{
+	  name: 'Jumpluff',
+	  types: ['grass', 'flying'],
+	  height: 0.8,
+	  weight: 3
+	},
+	{
+	  name: 'Articuno',
+	  types: ['ice', 'flying'],
+	  height: 1.7,
+	  weight: 55.4
+	},
+	{
+	  name: 'Mewtwo',
+	  types: []'psychic'],
+	  height: 2,
+	  weight: 122
+	},
+	{
+	  name: 'Luxray',
+	  types: ['electric'],
+	  height: 1.4,
+	  weight: 42
+	}
+];
