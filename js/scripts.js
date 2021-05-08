72354406b05f056496982660030ec63d4b02faf2
Long2589 <sarah.eliza.long@gmail.com>
2021-05-08 16:28:13 -0700

    Wrap pokemonList in IFEE and create pokemonRepository variable

--- js/scripts.js
+++ js/scripts.js
@@ -1,53 +1,61 @@
-let pokemonList = [
-	{
-	  name: 'Charizard',
-	  types: ['fire', 'flying'],
-	  height: 1.7,
-	  weight: 90.5
-	},
-	{
-	  name: 'Seadra',
-	  types: ['water'],
-	  height: 1.2,
-	  weight: 25
-	},
-	{
-	  name: 'Jumpluff',
-	  types: ['grass', 'flying'],
-	  height: 0.8,
-	  weight: 3
-	},
-	{
-	  name: 'Articuno',
-	  types: ['ice', 'flying'],
-	  height: 1.7,
-	  weight: 55.4
-	},
-	{
-	  name: 'Mewtwo',
-	  types: ['psychic'],
-	  height: 2,
-	  weight: 122
-	},
-	{
-	  name: 'Luxray',
-	  types: ['electric'],
-	  height: 1.4,
-	  weight: 42
-	}
-];
+var pokemonRespository = (function () {
+	var pokemonList = [
+		{
+		  name: 'Charizard',
+		  types: ['fire', 'flying'],
+		  height: 1.7,
+		  weight: 90.5
+		},
+		{
+		  name: 'Seadra',
+		  types: ['water'],
+		  height: 1.2,
+		  weight: 25
+		},
+		{
+		  name: 'Jumpluff',
+		  types: ['grass', 'flying'],
+		  height: 0.8,
+		  weight: 3
+		},
+		{
+		  name: 'Articuno',
+		  types: ['ice', 'flying'],
+		  height: 1.7,
+		  weight: 55.4
+		},
+		{
+		  name: 'Mewtwo',
+		  types: ['psychic'],
+		  height: 2,
+		  weight: 122
+		},
+		{
+		  name: 'Luxray',
+		  types: ['electric'],
+		  height: 1.4,
+		  weight: 42
+		}
+	];
 
-//For loop to iterate over objects in pokemonList array:
-//for (let i = 0; i < pokemonList.length; i++) {
-//Conditional to display message for tall pokemon:
-	//if (pokemonList[i].height >= 2) {
-		//document.write (pokemonList[i].name + " " + " (height: " + pokemonList[i].height + ")" + " Wow - that's a big Pokemon!" + "<br />");
-	//}else {
-		//document.write (pokemonList[i].name + " " + " (height: " + pokemonList[i].height + ")" + "<br />");
-	//}
-//}
+	function getAll() {
+		return pokemonList;
+	}
+
+	function add(pokemon) {
+		if(typeof pokemon === 'object'){
+		pokemonList.push(pokemon);
+		}
+	}
+
+	return {
+		getAll: getAll,
+		add: add
+	}
+})();
 
 //forEach() Loop:
-pokemonList.forEach(function(pokemon) {
+
+pokemonRespository.getAll().forEach(function(pokemon) {
 	document.write("<b>Name: </b>" + pokemon.name + " " + "<b>Type: </b>" + pokemon.types + " " + "<b>Height: </b>" + pokemon.height + " " + "<b>Weight: </b>" + pokemon.weight + "<br />");
 });
