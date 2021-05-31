42ddece2519a5111872e2f7a6ae53c5d0f57db96
Long2589 <sarah.eliza.long@gmail.com>
2021-05-30 18:37:36 -0700

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -30,20 +30,20 @@ let pokemonRepository = (function () {
 	}
 
 	function loadList() {
-	    return fetch(apiUrl).then(function (response) {
-	      	return response.json();
-	    }).then(function (json) {
-	        json.results.forEach(function (item) {
-		        let pokemon = {
-		            name: item.name,
-		            detailsUrl: item.url
-		        };
-		        add(pokemon);
-		        console.log(pokemon);
-            });
-        }).catch(function (e) {
-            console.error(e);
-        });
+	    	return fetch(apiUrl).then(function (response) {
+	      		return response.json();
+	    	}).then(function (json) {
+	        	json.results.forEach(function (item) {
+		        	let pokemon = {
+		            	name: item.name,
+		            	detailsUrl: item.url
+		        	};
+		        	add(pokemon);
+		        	console.log(pokemon);
+            	});
+        	}).catch(function (e) {
+            	console.error(e);
+       		 });
     }
 
 	function loadDetails(item) {
